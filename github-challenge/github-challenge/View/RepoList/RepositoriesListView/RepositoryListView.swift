38a3054e174a6d53f817e0ca19e4cc7ba1fc1383
Gerson Vieira <gersonr@MacBook-Pro-de-Gerson.local>
2020-02-18 22:26:26 -0300

    Cria main stack para adicionar lista de repositorios

--- github-challenge/github-challenge/View/RepoList/RepositoriesListView/RepositoryListView.swift
+++ github-challenge/github-challenge/View/RepoList/RepositoriesListView/RepositoryListView.swift
@@ -10,11 +10,22 @@ import UIKit
 
 class RepositoryListView: UIView {
     
+    var mainStack = UIStackView()
+    
     required init() {
         super.init(frame: CGRect(x: 0, y: 0, width: 10, height: 10))
+        setupStack()
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    func setupStack() {
+        mainStack.alignment = .fill
+        mainStack.axis = .vertical
+        mainStack.distribution = .fill
+        mainStack.spacing = 4.0
+    }
+    
 }
--- github-challenge/github-challenge/View/RepoList/RepositoryListViewController.swift
+++ github-challenge/github-challenge/View/RepoList/RepositoryListViewController.swift
@@ -9,8 +9,7 @@
 import UIKit
 
 class RepositoriesListViewController: BaseViewController<RepositoryListView> {
-    
-    private let api = RepositoriesProvider()
+     
     
     override func loadView() {
         super.loadView()
@@ -19,4 +18,33 @@ class RepositoriesListViewController: BaseViewController<RepositoryListView> {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    func reloadView(with data: [RepositoriesViewModel]) {
+        self.clearStack()
+        
+        for repo in data {
+            
+        }
+    }
+    
+    func clearStack() {
+        customView.mainStack.subviews.forEach({
+            $0.removeFromSuperview()
+        })
+    }
+}
+
+extension RepositoriesListViewController: RepositoriesManagerDelegate {
+    func didFetch(_ data: [RepositoriesViewModel]) {
+        self.reloadView(with: data)
+    }
+    
+    func errorToFetch(_ error: String) {
+        self.clearStack()
+        let alert = UIAlertController(title: "Alert", message: "Error: \(error)",         preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "Cancel", style: UIAlertAction.Style.default, handler: nil))
+       self.present(alert, animated: true, completion: nil)
+    }
+    
+    
 }
